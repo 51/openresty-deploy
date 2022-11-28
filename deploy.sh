bc5754ff3de42bf88c4575500cca8e02d93cc0fd
root <root@v.se>
2022-11-28 16:33:24 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+if [[ ! -f ./nginx/sites/default.conf ]]
+then
+   echo './nginx/sites/default.conf not found'
+   exit 0 
+fi
+
+if [[ ! -f /etc/ngx-openresty/conf.d/default.conf ]]
+then
+    cp ./nginx/sites/default.conf /etc/ngx-openresty/conf.d/default.conf
+fi
+
+if [[ ! -f /usr/local/sbin/nginx ]]
+then
+    cp ./nginx/nginx /usr/local/sbin/nginx
+    chmod +x /usr/local/sbin/nginx
+fi
+
+if [[ ! -d /opt/verynginx/configs/ ]]
+then
+    mkdir  -p /opt/verynginx/configs/
+fi
+
+if [[ ! -f /opt/verynginx/configs/config.json ]]
+then
+    cp ./nginx/verynginx/configs/config.json.example /opt/verynginx/configs/config.json
+fi
+
+if [[ ! -d /opt/www/ ]]
+then
+    mkdir  -p /opt/www/
+fi
+
+if [[ ! -d /opt/www/default ]]
+then
+    cp -r ./code/default /opt/www/
+fi
+
+if [[ ! -d /etc/cert/ ]]
+then
+    mkdir  -p /etc/cert/
+fi
+
+if [[ ! -d /etc/ngx-openresty ]]
+then
+    mkdir  -p /etc/ngx-openresty
+fi
+
+if [[ ! -d /var/log/nginx/ ]]
+then
+    mkdir  -p /var/log/nginx/
+fi
+
+docker compose up -d
